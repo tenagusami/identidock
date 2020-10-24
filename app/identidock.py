3e19bebf094372fc88ec4a699145a6f2d822f050
tenagusami <tenagusami_mail@yahoo.co.jp>
2020-10-24 17:07:08 +0900

    jenkins dokerfile is added.

--- app/identidock.py
+++ app/identidock.py
@@ -2,6 +2,7 @@ from flask import Flask, Response, request
 import requests
 import hashlib
 import redis
+import html
 
 
 app = Flask(__name__)
@@ -14,7 +15,7 @@ default_name = 'Joe Bloggs'
 def mainpage():
     name = default_name
     if request.method == 'POST':
-        name = request.form['name']
+        name = html.escape(request.form['name'], quote=True)
     salted_name = salt + name
     name_hash = hashlib.sha256(salted_name.encode()).hexdigest()
     header = '<html><head><title>Identidock</title></head><body>'
@@ -31,6 +32,7 @@ def mainpage():
 
 @app.route('/monster/<name>')
 def get_identicon(name):
+    name = html.escape(name, quote=True)
     image = cache.get(name)
     if image is None:
         print("Cache miss", flush=True)
